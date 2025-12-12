644a78c0c467e666c5de081327e84201a946768b
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-12 20:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species=species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound():void{
+		console.log("The"+this._species+"makes a sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark():void{
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr():void{
+		console.log("purr");
+	}
+}
+const myCat=new cat("Siamese");
+myCat.makeSound();
+myCat.purr();
+
+const myDog=new Dog("Golden Retriever");
+myDog.makeSound();
+myDog.bark();
+
 
 // Do not change the code below this line
 window.Animal = Animal;
